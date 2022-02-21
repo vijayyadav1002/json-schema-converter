d46e62ae95723b03d3f75fe68d0990c0ff11d002
sg0227929 <vijay.yadav@sabre.com>
2022-02-21 13:55:01 -0600

    Added required property

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,14 @@ import './App.css';
 
 function App(): JSX.Element {
     const [error, setError] = useState('');
+    const [required, setRequired] = useState(true);
     const [jsonInput, setJsonInput] = useState('{"name": "Dan"}');
     const [schemaInput, setSchemaInput] = useState('{}');
     const [isChecked, setIsChecked] = useState(true);
     const convertToSchema = (event: any) => {
         event && event.preventDefault();
         try {
-            const schema = toJsonSchema(JSON.parse(jsonInput));
+            const schema = toJsonSchema(JSON.parse(jsonInput), {required});
             if (isChecked) {
                 schema.additionalProperties = isChecked;
             }
@@ -37,6 +38,10 @@ function App(): JSX.Element {
                     <label htmlFor='additional-properties'>
                         <input type='checkbox' checked={isChecked} id='additional-properties' onChange={() => setIsChecked(!isChecked)}/>
                         <span>Additional Properties</span>
+                    </label>
+                    <label htmlFor='all-required'>
+                        <input type='checkbox' checked={required} id='all-required' onChange={() => setRequired(!required)}/>
+                        <span>All Required</span>
                     </label>
                 </div>
                 <textarea rows={20} cols={50} value={schemaInput}/>
